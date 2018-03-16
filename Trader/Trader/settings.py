767707d21a70c7ec9bd92c2f138c05a653c8bc2c
jatin58 <jatingoel58@gmail.com>
2018-03-17 02:02:09 +0530

    Postgres Database setup

--- Trader/Trader/settings.py
+++ Trader/Trader/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Trader.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'broker',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
